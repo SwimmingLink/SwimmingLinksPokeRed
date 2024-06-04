b241ed2a06105a8d9d424de1a068a16015fed5d1
SwimmingLink <34524859+SwimmingLink@users.noreply.github.com>
2024-06-04 15:24:05 -0500

    Update animations.asm
    Keep the lower-right tile of sprites from deleting as they slide off the screen

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -1837,7 +1837,7 @@ _AnimationSlideMonOff:
 ; This is a bug. The lower right corner tile of the mon back pic is blanked
 ; while the mon is sliding off the screen. It should compare with the max tile
 ; plus one instead.
-	cp $61
+	cp $62 ; This was edited from 61 to 62 to fix the commented issue above
 	ret c
 	ld a, " "
 	ret
@@ -1847,7 +1847,7 @@ _AnimationSlideMonOff:
 	sub 7
 ; This has the same problem as above, but it has no visible effect because
 ; the lower right tile is in the first column to slide off the screen.
-	cp $30
+	cp $31 ; This was edited from 30 to 31 to fix the commented issue above
 	ret c
 	ld a, " "
 	ret
